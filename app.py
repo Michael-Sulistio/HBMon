881912727c2a7b2f1609e8f19686e8c3c48d8d9b
Michael-Sulistio <157099905+Michael-Sulistio@users.noreply.github.com>
2024-06-10 21:12:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ model = tf.keras.models.load_model('ML_Program', custom_objects=custom_objects)
 
 @app.route('/')
 def home():
-    return "HB prediction server"
+    return "Hb prediction server"
 
 @app.route('/predict', methods=['POST'])
 def predict():
